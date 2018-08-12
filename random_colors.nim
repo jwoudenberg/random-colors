d1c15fc2089ccec2f4f6101ef60613f4404ac851
Jasper Woudenberg <mail@jasperwoudenberg.com>
2018-08-12 14:37:53 +0200

    Get scheme from colormind.io and parse response

--- random_colors.nim
+++ random_colors.nim
@@ -2,9 +2,14 @@ from os import nil
 from osproc import nil
 from strutils import `%`
 from not_nil import prove
+from httpclient import nil
+import json
 
 type
   Location = distinct (string not nil)
+  ColorValue = range[0..256]
+  Color = tuple[red: ColorValue, green: ColorValue, blue: ColorValue]
+  Scheme = tuple[foreground: Color, light: Color, main: Color, dark: Color, background: Color]
 
 
 proc with_default(str : string, default : string not nil): string not nil {.noSideEffect.} =
@@ -48,5 +53,27 @@ proc encode(str : string not nil): Location {.noSideEffect.} =
       add(encoded, "_$1_" % strutils.toHex(c))
   return (Location encoded)
 
+proc parse_color_value(value: JsonNode): ColorValue =
+  return ColorValue(getNum(value))
 
-echo string(encode(key()))
+proc parse_color(color: JsonNode): Color =
+  return (
+    red: parse_color_value(color[0]),
+    green: parse_color_value(color[1]),
+    blue: parse_color_value(color[2])
+  )
+
+proc request_colors(loc : Location): Scheme =
+  let client = httpclient.newHttpClient()
+  var body = %* {"model": "ui"}
+  let response = httpclient.postContent(client, "http://colormind.io/api/", pretty(body))
+  let json = parseJson(response)
+  return (
+    foreground: parse_color(json["result"][0]),
+    light: parse_color(json["result"][1]),
+    main: parse_color(json["result"][2]),
+    dark: parse_color(json["result"][3]),
+    background: parse_color(json["result"][4])
+  )
+
+echo request_colors(encode(key()))
